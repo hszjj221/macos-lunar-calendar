8c1e2ab23220e655b624633ea74629733115bf99
hszjj221 <hszjj221@live.com>
2026-03-12 10:49:24 +0800

    fix: 日历打开时自动刷新日期到当天
    在 CalendarPopoverView 添加 .onAppear 修饰符，每次打开 popover 时
    自动将 displayDate 设置为当前日期，解决跨天后仍显示昨天的问题。
    
    - 更新版本号到 1.1

--- Sources/CalendarApp/Views/CalendarPopoverView.swift
+++ Sources/CalendarApp/Views/CalendarPopoverView.swift
@@ -69,6 +69,9 @@ struct CalendarPopoverView: View {
             .padding(.bottom, 8)
         }
         .frame(width: 320)
+        .onAppear {
+            displayDate = Date()
+        }
     }
 
     private var monthYearTitle: String {
